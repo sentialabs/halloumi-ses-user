ea28d2dcd8b0391c21d393069dbc24fb54fda1c3
Rafael Z. Kineippe <rafael.zamana.kineippe@sentia.com>
2021-03-08 15:35:13 +0100

    fix: Improving tests

--- test/add-user.test.ts
+++ test/add-user.test.ts
@@ -1,10 +1,11 @@
 
-import { expect, haveResource } from '@aws-cdk/assert';
+import { expect, haveResource, countResources } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import { SesUser } from '../src';
 
-test('addUser', () => {
+test('creates the user', () => {
   const stack = new cdk.Stack();
   new SesUser(stack, 'User');
   expect(stack).to(haveResource('AWS::IAM::User'));
+  expect(stack).to(countResources('AWS::IAM::User', 1));
 });
