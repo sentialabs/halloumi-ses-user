7dccf9decc656ead79718fecece5a5186a4dfa07
Rafael Z. Kineippe <rafael.zamana.kineippe@sentia.com>
2021-03-12 16:53:17 +0100

    fix(tests): Adding internal tests

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -1,6 +1,15 @@
 import { CloudFormationCustomResourceEvent, Context } from 'aws-lambda'; // eslint-disable-line import/no-unresolved
 import * as cfnResp from 'cfn-response';
 
+function response(event: CloudFormationCustomResourceEvent, context: Context, message: string, err?: any): Promise<string> {
+  return new Promise(() => {
+    let status: cfnResp.ResponseStatus = 'SUCCESS';
+    if (err) { console.log(event, context, err); status = 'FAILED'; } else {console.log(message);}
+    if (Object.prototype.hasOwnProperty.call(event, 'RequestType')) return cfnResp.send(event, context, status, err ? { error: err } : { message }, event.LogicalResourceId);
+    return status;
+  });
+}
+
 function on_create(event: CloudFormationCustomResourceEvent, context: Context) {
   response(event, context, 'Success').then(data => {
     console.log(data);
@@ -17,17 +26,8 @@ function on_delete(event: CloudFormationCustomResourceEvent) {
 
 }
 
-function response(event: CloudFormationCustomResourceEvent, context: Context, message: string, err?: any): Promise<string> {
-  return new Promise(() => {
-    let status: cfnResp.ResponseStatus = 'SUCCESS';
-    if (err) { console.log(event, context, err); status = 'FAILED'; } else {console.log(message);}
-    if (Object.prototype.hasOwnProperty.call(event, 'RequestType')) return cfnResp.send(event, context, status, err ? { error: err } : { message }, event.LogicalResourceId);
-    return status;
-  });
-}
-
-export const on_event = (event: CloudFormationCustomResourceEvent, context: Context) => {
-  // console.log(event);
+export function on_event(event: CloudFormationCustomResourceEvent, context: Context) {
+  console.log(event);
   console.log(context);
   const request_type = event.RequestType;
   if (request_type == 'Create') { return on_create(event, context);}
--- test/lambda.test.ts
+++ test/lambda.test.ts
@@ -1,15 +1,34 @@
-import * as cfnResp from 'cfn-response';
-import { on_event } from '../src/lambda/index';
+import cfnResp from 'cfn-response';
+import rewire from 'rewire';
+import * as _lambda from '../src/lambda/index';
 import { constructCFNCRREvent, constructCFNCRRContext } from './utils/helpers';
 
-describe('Test Lambda', () => {
-  it('should return True', async () => {
-    const addMock = jest.spyOn(cfnResp, 'send').mockImplementation();
+jest.spyOn(global.console, 'log').mockImplementation();
+const _lambdaModule = rewire('../lib/lambda/index');
+_lambdaModule.__set__('console', {
+  log: jest.fn(() => {}),
+});
 
+describe('Test Lambda', () => {
+  it('should call on_create', async () => {
+    const mockOnCreate = jest.fn(() => {});
+    const revert = _lambdaModule.__set__('on_create', mockOnCreate);
     const event = constructCFNCRREvent();
     const context = constructCFNCRRContext();
-    on_event(event, context);
-    expect(addMock).toHaveBeenCalled();
-    expect(addMock.mock.calls[0][2]).toBe('SUCCESS');
+
+    _lambdaModule.on_event(event, context);
+    expect(mockOnCreate).toHaveBeenCalled();
+
+    revert();
+  });
+
+  it('should send', async () => {
+    const cfnRespSendMock = jest.spyOn(cfnResp, 'send').mockImplementation();
+    const event = constructCFNCRREvent();
+    const context = constructCFNCRRContext();
+
+    _lambda.on_event(event, context);
+    expect(cfnRespSendMock).toHaveBeenCalled();
+    expect(cfnRespSendMock.mock.calls[0][2]).toBe('SUCCESS');
   });
 });
