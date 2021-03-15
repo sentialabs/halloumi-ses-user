cc20e9fb8af4249c820230839b558e9adadc3def
Rafael Z. Kineippe <rafael.zamana.kineippe@sentia.com>
2021-03-15 09:53:11 +0100

    feat(CustomResource): Adding Role to use the secrets

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { User, UserProps, CfnAccessKey } from '@aws-cdk/aws-iam';
+import { User, UserProps, CfnAccessKey, Role, ServicePrincipal, PolicyStatement, Effect } from '@aws-cdk/aws-iam';
 import { CfnSecret } from '@aws-cdk/aws-secretsmanager';
 import { Construct, Fn } from '@aws-cdk/core';
 export class SesUser extends User {
@@ -11,11 +11,28 @@ export class SesUser extends User {
 
     const secret_key = Fn.getAtt(access_keys.logicalId, 'SecretAccessKey');
 
-    new CfnSecret(scope, `${id}Secret`, {
+    const secret = new CfnSecret(scope, `${id}Secret`, {
       secretString: JSON.stringify({
         access_keys: access_keys.ref,
         secret_key: secret_key.toString(),
       }),
     });
+
+    const _lambda_role = new Role(scope, `${id}Role`, {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    _lambda_role.addToPrincipalPolicy(new PolicyStatement({
+      actions: [
+        "secretsmanager:GetResourcePolicy",
+        "secretsmanager:GetSecretValue",
+        "secretsmanager:DescribeSecret",
+        "secretsmanager:ListSecretVersionIds"
+      ],
+      effect: Effect.ALLOW,
+      resources:[
+        secret.ref
+      ]
+    }))
   }
 }
--- test/add-user.test.ts
+++ test/add-user.test.ts
@@ -1,10 +1,10 @@
 import { expect, haveResource, countResources } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
+import { Stack } from '@aws-cdk/core';
 import { SesUser } from '../src';
 
 describe('User', () => {
   it('creates the user', () => {
-    const stack = new cdk.Stack();
+    const stack = new Stack();
     new SesUser(stack, 'User');
     let secret = {
       'Fn::Join': [
@@ -32,7 +32,36 @@ describe('User', () => {
     expect(stack).to(haveResource('AWS::SecretsManager::Secret', {
       SecretString: secret,
     }));
-    expect(stack).to(haveResource('AWS::IAM::Role'));
     expect(stack).to(countResources('AWS::IAM::Role', 1));
+    expect(stack).to(haveResource('AWS::IAM::Role', {
+      AssumeRolePolicyDocument: {
+        Statement: [{
+          Effect: 'Allow',
+          Principal: { Service: 'lambda.amazonaws.com' },
+          Action: 'sts:AssumeRole',
+        }],
+        Version: '2012-10-17',
+      }
+    }));
+    expect(stack).to(countResources('AWS::IAM::Policy', 1));
+    expect(stack).to(haveResource('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Effect: "Allow",
+            Action: [
+              "secretsmanager:GetResourcePolicy",
+              "secretsmanager:GetSecretValue",
+              "secretsmanager:DescribeSecret",
+              "secretsmanager:ListSecretVersionIds"
+            ],
+            Resource: {
+              "Ref": "UserSecret"
+            }
+          }
+        ]
+      }
+    }));
   });
 });
