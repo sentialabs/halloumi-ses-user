7309ce52dcd6d054bcb0934c3c3b9b7ab34001ba
Rafael Z. Kineippe <rafael.zamana.kineippe@sentia.com>
2021-03-15 09:18:07 +0100

    feat(secret): Adding a secret with AccessKey and SecretKey

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import { User, UserProps, CfnAccessKey } from '@aws-cdk/aws-iam';
-import { Construct } from '@aws-cdk/core';
+import { CfnSecret } from '@aws-cdk/aws-secretsmanager';
+import { Construct, Fn } from '@aws-cdk/core';
 export class SesUser extends User {
   constructor(scope: Construct, id: string, props?: UserProps) {
     super(scope, id, props);
 
-    new CfnAccessKey(scope, `${id}AccessKeys`, {
+    const access_keys = new CfnAccessKey(scope, `${id}AccessKeys`, {
       userName: this.userName,
+    });
+
+    const secret_key = Fn.getAtt(access_keys.logicalId, 'SecretAccessKey');
+
+    new CfnSecret(scope, `${id}Secret`, {
+      secretString: JSON.stringify({
+        access_keys: access_keys.ref,
+        secret_key: secret_key.toString(),
+      }),
     });
   }
 }
--- test/add-user.test.ts
+++ test/add-user.test.ts
@@ -6,13 +6,31 @@ describe('User', () => {
   it('creates the user', () => {
     const stack = new cdk.Stack();
     new SesUser(stack, 'User');
+    let secret = {
+      'Fn::Join': [
+        '',
+        [
+          '{"access_keys":"',
+          {
+            Ref: 'UserAccessKeys',
+          },
+          '","secret_key":"',
+          {
+            'Fn::GetAtt': [
+              'UserAccessKeys',
+              'SecretAccessKey',
+            ],
+          },
+          '"}',
+        ],
+      ],
+    };
     expect(stack).to(haveResource('AWS::IAM::User'));
     expect(stack).to(countResources('AWS::IAM::User', 1));
-  });
-  it('creates the access_key', () => {
-    const stack = new cdk.Stack();
-    new SesUser(stack, 'User');
     expect(stack).to(haveResource('AWS::IAM::AccessKey'));
     expect(stack).to(countResources('AWS::IAM::AccessKey', 1));
+    expect(stack).to(haveResource('AWS::SecretsManager::Secret', {
+      SecretString: secret,
+    }));
   });
 });
